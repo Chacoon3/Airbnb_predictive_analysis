529a220319783eab8460d0cf00f5388aaf6dc064
Q ! <quintumadastra@gmail.com>
2023-05-06 16:40:33 -0400

    updated tree models

--- tree_model_updated.r
+++ tree_model_updated.r
@@ -1,40 +1,29 @@
 # This line references the data_cleaning.r script so that you can call functions that are written in that r file. 
-# Libraries called in the referenced file will automatically be included in this file.
+# Libraries called in the referenced file will automatically be included in this file
 source('Library\\data_cleaning.r')
-
+source('Library\\utils.r')
+library(xgboost)
+library(ranger)
+library(caret)
+library(glmnet)
+options(scipen = 999)
 # replace the string with the directory of the project folder "Data"
 # the original datasets MUST be placed under the Data folder for the following script to run correctly.
 folder_dir = r"(C:\Users\quinc\OneDrive - University of Maryland\My Drive\College\5PlusOne\Semester 2\BUDT758T MIN\Final Project\Airbnb_predictive_analysis\Data)"
 
-# This line needs only to be run once, which exports two csv files, one for the training X's, the other for the testing X's. Once the two files were created you only need to run the read.csv statements under this line.
-export_cleaned(folder_dir)
-
 
 # read
-x_train <- read.csv('Data\\x_train_clean.csv')
-x_test <- read.csv('Data\\x_test_clean.csv')
+x_full_set <- get_cleaned(folder_dir)
+
 y_train <- read.csv('Data\\airbnb_train_y_2023.csv')
-hbr <- y_train$high_booking_rate
-prs <- y_train$perfect_rating_score
 
 
-# test
-nrow(x_train) == nrow(y_train)
-nrow(x_test) == 12205
-(length(hbr) == length(prs)) && (length(hbr) == nrow(y_train))
-
-
-# train-validation split
-sampled = sample(1:nrow(x_train), 0.75 * nrow(x_train))
-x_tr = x_train[sampled, ]
-x_va = x_train[-sampled, ]
-hbr_tr = hbr[sampled]
-hbr_va = hbr[-sampled]
-prs_tr = prs[sampled]
-prs_va = prs[-sampled]
-
-
-# codes start here ----------------------------
+hbr <- y_train$high_booking_rate %>% as.factor()
+hbr <- ifelse(hbr == 'YES', 1, 0)
+prs <- y_train$perfect_rating_score %>% as.factor()
+prs <- ifelse(prs == 'YES', 1, 0)
+x_train <- x_full_set[1:nrow(y_train),]
+x_test <- x_full_set[(nrow(y_train) + 1): nrow(x_full_set),]
 
 
 
@@ -42,30 +31,23 @@ prs_va = prs[-sampled]
 ##############QUINN TREE MODEL CODE BELOW##################
 
 
-#xy_train <- cbind(x_train, y_train_hbr)
-#xy_train <- cbind(xy_train,y_train_prs)
-#summary(xy_train)
 
 library(tree)
 
-#valid_instn = sample(nrow(x_train), 0.30*nrow(x_train))
-#x_train_va <- x_train[valid_instn,]
-#x_train_tr <- x_train[-valid_instn,]
-#y_train_hbr_va <- y_train_hbr[valid_instn]
-#y_train_hbr_tr <- y_train_hbr[-valid_instn]
+
+#xy_train <- cbind(x_train, hbr)
 
 
-xy_train <- cbind(x_train, y_train)
+sampled = sample(1:nrow(x_train), 0.75 * nrow(x_train))
+x_tr = x_train[sampled, ]
+x_va = x_train[-sampled, ]
+hbr_tr = hbr[sampled]
+hbr_va = hbr[-sampled]
 
-train_inst <- sample(nrow(xy_train), .7*nrow(xy_train))
-train <- xy_train[train_inst,]
-valid <- xy_train[-train_inst,]
 
+mycontrol = tree.control(nrow(x_tr), mincut = 5, minsize = 10, mindev = 0.0005)
 
-
-mycontrol = tree.control(nrow(train), mincut = 5, minsize = 10, mindev = 0.0005)
-
-full_tree=tree(y_train ~ accommodates + availability_30 + 
+full_tree=tree(hbr_tr ~ accommodates + availability_30 + 
                  availability_365 + availability_60 + availability_90 + bathrooms+
                  bed_type + bedrooms + beds + cancellation_policy+
                  cleaning_fee+experiences_offered+extra_people +
@@ -84,20 +66,20 @@ full_tree=tree(y_train ~ accommodates + availability_30 +
                  zhima_selfie+has_interaction+is_note+property_category+has_about+
                  host_acceptance+has_host_name+host_response+has_house_rules,
                #had to remove all character vars, all vars with factor levels>32, all vars with spaces in name
-               data = train, control = mycontrol)
+               data = x_tr, control = mycontrol)
 summary(full_tree)
 plot(full_tree)
 text(full_tree,pretty=1)
 
-##code to make the one node tree
-#pruned_tree_1=prune.tree(full_tree, best = 1)
-#summary(pruned_tree_1)
-#plot(pruned_tree_1)
-#text(pruned_tree_1,pretty=1)
-
-#pruned_tree_1
+##5/1: code to make the tree with one split
+pruned_tree_1=prune.tree(full_tree, best = 2)
+summary(pruned_tree_1)
+plot(pruned_tree_1)
+text(pruned_tree_1,pretty=1)
 
 
+
+############5/1: CODE BELOW HAS NOT BEEN FIXED TO MATCH WITH VARIABLE CHANGES YET
 
 
 accuracy <- function(classifications, actuals){
